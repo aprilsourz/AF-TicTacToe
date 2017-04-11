fbc11bd1f7680fd5a9dd5fb3ad05c0891994ada3
arlofeirman <arlofeirman@gmail.com>
2017-04-11 14:52:30 -0400

    Adds unbind function for game board click events
    Player cannot change a cell after it has been filled.

--- assets/scripts/game-logic/events.js
+++ assets/scripts/game-logic/events.js
@@ -9,6 +9,7 @@ const addHandlers = () => {
     const cellChoice = '#cell-0'
     ui.cellFill(player, cellChoice)
     player = !player
+    $('#cell-0').unbind('click')
   })
 
   $('#cell-1').click(() => {
@@ -16,6 +17,7 @@ const addHandlers = () => {
     const cellChoice = '#cell-1'
     ui.cellFill(player, cellChoice)
     player = !player
+    $('#cell-1').unbind('click')
   })
 
   $('#cell-2').click(() => {
@@ -23,6 +25,7 @@ const addHandlers = () => {
     const cellChoice = '#cell-2'
     ui.cellFill(player, cellChoice)
     player = !player
+    $('#cell-2').unbind('click')
   })
 
   $('#cell-3').click(() => {
@@ -30,6 +33,7 @@ const addHandlers = () => {
     const cellChoice = '#cell-3'
     ui.cellFill(player, cellChoice)
     player = !player
+    $('#cell-3').unbind('click')
   })
 
   $('#cell-4').click(() => {
@@ -37,6 +41,7 @@ const addHandlers = () => {
     const cellChoice = '#cell-4'
     ui.cellFill(player, cellChoice)
     player = !player
+    $('#cell-4').unbind('click')
   })
 
   $('#cell-5').click(() => {
@@ -44,6 +49,7 @@ const addHandlers = () => {
     const cellChoice = '#cell-5'
     ui.cellFill(player, cellChoice)
     player = !player
+    $('#cell-5').unbind('click')
   })
 
   $('#cell-6').click(() => {
@@ -51,6 +57,7 @@ const addHandlers = () => {
     const cellChoice = '#cell-6'
     ui.cellFill(player, cellChoice)
     player = !player
+    $('#cell-6').unbind('click')
   })
 
   $('#cell-7').click(() => {
@@ -58,6 +65,7 @@ const addHandlers = () => {
     const cellChoice = '#cell-7'
     ui.cellFill(player, cellChoice)
     player = !player
+    $('#cell-7').unbind('click')
   })
 
   $('#cell-8').click(() => {
@@ -65,6 +73,7 @@ const addHandlers = () => {
     const cellChoice = '#cell-8'
     ui.cellFill(player, cellChoice)
     player = !player
+    $('#cell-8').unbind('click')
   })
 }
 
