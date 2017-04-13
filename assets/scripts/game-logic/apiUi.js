77c72fe435188ce36c89f4c16e498ccd31c5b711
arlofeirman <arlofeirman@gmail.com>
2017-04-13 16:03:21 -0400

    add UI for displaying stats

--- assets/scripts/game-logic/apiUi.js
+++ assets/scripts/game-logic/apiUi.js
@@ -3,36 +3,39 @@
 const store = require('../store.js')
 
 const createGameSuccess = (data) => {
-  console.log(data)
   store.game = data.game.id
-  console.log(store.game)
 }
 
 const createGameFailure = () => {
-  console.log('error')
+  )
 }
 
 const updateGameSuccess = (data) => {
-  console.log(data.game.over, data.game.cells)
+
 }
 
 const updateGameFailure = () => {
-  console.log('error')
+
 }
 
 const getGamesSuccess = (data) => {
-  console.log('here are all the games', data)
+  $('#display-stats').text('You have played ' + data.games.length + ' games of tic tac toe so far!')
 }
 
 const getGamesFailure = () => {
-  console.log('error')
+
 }
 
 const oneGameSuccess = (data) => {
   console.log('one game', data)
+  if (data.game.over) {
+    $('#display-stats').text('Game number ' + data.game.id + ' was completed')
+  } else {
+    $('#display-stats').text('Game number ' + data.game.id + ' was not completed')
+  }
 }
 const oneGameFailure = (data) => {
-  console.log('error')
+  $('#display-stats').text('Please enter a valid ID!')
 }
 module.exports = {
   createGameSuccess,
