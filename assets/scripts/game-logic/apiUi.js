929bb0c7326d82494f9545b4fb5e97bf2355bded
arlofeirman <arlofeirman@gmail.com>
2017-04-13 16:06:43 -0400

    Fixes bug in Stats UI

--- assets/scripts/game-logic/apiUi.js
+++ assets/scripts/game-logic/apiUi.js
@@ -3,27 +3,30 @@
 const store = require('../store.js')
 
 const createGameSuccess = (data) => {
+  console.log(data)
   store.game = data.game.id
+  console.log(store.game)
 }
 
 const createGameFailure = () => {
-  )
+  console.log('error')
 }
 
 const updateGameSuccess = (data) => {
-
+  console.log(data.game.over, data.game.cells)
 }
 
 const updateGameFailure = () => {
-
+  console.log('error')
 }
 
 const getGamesSuccess = (data) => {
+  console.log('here are all the games', data)
   $('#display-stats').text('You have played ' + data.games.length + ' games of tic tac toe so far!')
 }
 
 const getGamesFailure = () => {
-
+  $('#display-stats').text('Please Sign in!')
 }
 
 const oneGameSuccess = (data) => {
